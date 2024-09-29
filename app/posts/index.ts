bc9dd68b959031e7cb850fd6ca1afe4517682e58
Jaime Liz <lizjaime@gmail.com>
2024-09-29 14:16:43 -0500

    feat: update post attributes

--- app/posts/index.ts
+++ app/posts/index.ts
@@ -4,15 +4,21 @@ import Test, {
 } from './test.mdx'
 
 interface Posts {
-  title: string
+  published: number
   slug: string
+  tags?: string[]
+  title: string
+  preview: string
   content: React.ElementType
 }
 
 export const posts: Array<Posts> = [
   {
+    published: testAttr.published,
     slug: testAttr.slug,
+    tags: testAttr.tags,
     title: testAttr.title,
+    preview: testAttr.preview,
     content: Test,
   },
 ]
