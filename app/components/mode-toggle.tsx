c810a469f5f129ace982e1a2d52f1b38148a2a96
Jaime Liz <lizjaime@gmail.com>
2024-10-06 18:40:15 -0500

    fix: hydration error

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -1,30 +1,40 @@
 import { Moon, Sun } from 'lucide-react'
-import { useMemo } from 'react'
 import { Theme, useTheme } from 'remix-themes'
-import { Toggle } from '~/components/ui/toggle.tsx'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
 
 export function ModeToggle() {
-  const [theme, setTheme] = useTheme()
-
-  const Icon = useMemo(() => {
-    if (theme === 'light') {
-      return Sun
-    }
-    return Moon
-  }, [theme])
+  const [, setTheme] = useTheme()
 
   return (
-    <Toggle
-      aria-label="Toggle theme"
-      onClick={() =>
-        setTheme((theme) =>
-          theme === Theme.DARK
-            ? Theme.LIGHT
-            : Theme.DARK,
-        )
-      }
-    >
-      <Icon />
-    </Toggle>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">
+            Toggle theme
+          </span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem
+          onClick={() => setTheme(Theme.LIGHT)}
+        >
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => setTheme(Theme.DARK)}
+        >
+          Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
